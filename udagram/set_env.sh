f307e6017da05554863e8d5777e657cb02ec3c6d
InMyListk <InMyListk@gmail.com>
2023-04-12 10:40:48 -0700

    Initial commit

---
+++ udagram/set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=535286482
+export POSTGRES_HOST=deploy.cyev3hbw9gyo.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=deploy
+export AWS_BUCKET=elasticbeanstalk-us-east-1-585718443922
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=Deploy5352
+export URL=http://localhost:8100
